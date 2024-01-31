dd0381ada88a9e89410096c5dee4499f38c7f8b0
unknown <nano343kk4@gmail.com>
2024-01-31 17:24:40 +0700

    Updateee

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Product extends Model
 {
     use HasFactory;
+    protected $primaryKey = 'products_id';
+
+    public function category(){
+        return $this->belongts(Category::class,'category_id');
+
+
+    }
 }
